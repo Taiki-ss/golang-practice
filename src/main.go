22dac2e07d2957775c5e7031fe9572bb6ab02577
Taiki-nk <taikifate.0206@gmail.com>
2023-02-12 00:43:32 +0900

    struct practice

--- src/main.go
+++ src/main.go
@@ -2,40 +2,18 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func handler(writer http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(writer, "Hello World")
+type User struct {
+	name string
+}
+
+func (user User) cal(weight float32, height float32) (bmi float32) {
+	bmi = weight / height / height * 10000
+	return
 }
 
 func main() {
-	// http.HandleFunc("/", handler)
-	// http.ListenAndServe(":8080", nil)
-
-	f := 1.11
-	fmt.Printf("%d\n", int(f))
-
-	m := map[string]int{
-		"Mike":  20,
-		"Nancy": 24,
-		"Messi": 30,
-	}
-	fmt.Printf("%T %v\n", m, m)
-
-	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
-
-	var minNumber int
-	for i := 0; i < len(l); i++ {
-		if i == 0 {
-			minNumber = l[i]
-			continue
-		}
-
-		if minNumber > l[i] {
-			minNumber = l[i]
-		}
-	}
-
-	fmt.Println(minNumber)
+	user := User{name:"taiki"}
+	fmt.Printf("%sのBIMは%gです。", user.name, user.cal(68,177))
 }
