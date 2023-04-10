876d969cac62239e999c793fadc569dcfb479a80
Taiki-nk <taikifate.0206@gmail.com>
2023-04-10 22:35:58 +0900

    runtimeパッケージテスト

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 type User struct {
@@ -14,6 +15,12 @@ func (user User) cal(weight float32, height float32) (bmi float32) {
 }
 
 func main() {
+	// BMI出力
 	user := User{name:"taiki"}
 	fmt.Printf("%sのBIMは%gです。", user.name, user.cal(68,177))
+
+	// 使用中のOS出力
+	os := runtime.GOOS;
+	fmt.Println(os)
 }
+
